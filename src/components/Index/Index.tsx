7d3bd7ddfde8dda46684825bc5fbd19a7060f090
KaiKai <1035759181@qq.com>
2019-03-26 16:04:23 +0800

    Add React-Router

---
+++ src/components/Index/Index.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { Button } from 'antd'
+
+interface IRouter {
+    history: any;
+}
+
+class Component extends React.Component<IRouter> {
+    constructor(props: any){
+        super(props)
+    }
+
+    login = ()=> {
+        this.props.history.push('/login')
+    }
+
+
+    render(){
+        return(
+        <div className="Component">
+            Index
+            <Button onClick={this.login}>登录</Button>
+        </div>
+        )
+  }
+}
+
+export default Component;
